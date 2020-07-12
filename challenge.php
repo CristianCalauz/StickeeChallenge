674002460eb566ca47540f23673e5d6360261bd2
Cristian Calauz <cristiancalauz98@gmail.com>
2020-07-12 15:15:18 +0100

    clarity and efficiency changes

--- challenge.php
+++ challenge.php
@@ -11,42 +11,47 @@ $widgetPacks = array(250, 500, 1000, 2000, 5000);
 
 // Some preprocessing
 array_unique($widgetPacks);
-define("ARRAYLENGTH", count($widgetPacks));
 rsort($widgetPacks);
+define("ARRAYLENGTH", count($widgetPacks));
+define("SMALLESTPACK", $widgetPacks[ARRAYLENGTH - 1]);
+define("BIGGESTPACK", $widgetPacks[0]);
 
 echo "<br>Packs to be ordered:<br>";
 if($order > 0) {
 	// If the order is smaller than the smallest pack, order the smallest pack
-	if($order < $widgetPacks[ARRAYLENGTH - 1]) {
-		echo $widgetPacks[ARRAYLENGTH - 1] . " X 1";
+	if($order < SMALLESTPACK) {
+		echo SMALLESTPACK . " X 1";
 	} else {
-		// If the order is bigger than the biggest pack, order the biggest one until others should be ordered
-		$bigOrder = array();
-		while($order > $widgetPacks[0]) {
-			array_push($bigOrder, $widgetPacks[0]);
-			$order -= $widgetPacks[0];
-		}
-		
+		// If the order is bigger than the biggest pack, order the big ones until others should be ordered
+		$bigPacks = floor($order / $widgetPacks[0]);
+		$order -= $bigPacks * $widgetPacks[0];
+	
 		// This is where the array that keeps track of all the boxes is made
 		$smallOrder = packsToSend($order, 0, $widgetPacks);
-		for ($i = 0; $i < count($smallOrder); $i++) {
-			array_push($bigOrder, $smallOrder[$i]);
-		}
+		rsort($smallOrder);
 		
-		// Output algorithm
-		rsort($bigOrder);
-		$counter = 1;
-		$i = 0;
-		while ($i < count($bigOrder) - 1) {
-			if ($bigOrder[$i] == $bigOrder[$i + 1]) {
-				$counter++;
-			} else {
-				echo $bigOrder[$i] . " X " . $counter . "<br>";
-				$counter = 1;
+		// Output algorithm. 
+		// If the first element of smallOrder is the biggest box, that means only one box was ordered, thus the number of boxes is bigPacks + 1
+		if($smallOrder[0] == BIGGESTPACK) {
+			echo BIGGESTPACK . "X" . ($bigPacks + 1) . "<br>";
+		// Otherwise the number of big boxes is bigPacks.
+		} else {
+			if($bigPacks != 0) {
+				echo BIGGESTPACK . "X" . $bigPacks . "<br>";
 			}
-			$i++;
+			$counter = 1;
+			$i = 0;
+			while ($i < count($smallOrder) - 1) {
+				if ($smallOrder[$i] == $smallOrder[$i + 1]) {
+					$counter++;
+				} else {
+					echo $smallOrder[$i] . " X " . $counter . "<br>";
+					$counter = 1;
+				}
+				$i++;
+			}
+			echo $smallOrder[$i] . " X " . $counter . "<br>";
 		}
-		echo $bigOrder[$i] . " X " . $counter . "<br>";
 	}
 } else {
 	echo "Incorrect input";
@@ -56,38 +61,38 @@ if($order > 0) {
 // This algorithm recursively assumes that the box that is smaller than the order will be bought, until the end, where it chooses to either buy 2 
 // of the smallest boxes, or one bigger box. After the recursion calls, while going back down the stack, the algorithm checks if it was worth buying
 // more small boxes, than one of bigger size.
-function packsToSend ($order, $iterator, $widgetPacks) {
-	// The iterator represents a position which shows the box that is bigger than the order, and the one that is smaller than the order
-	while ($widgetPacks[$iterator] > $order) {
-		$iterator++;
+function packsToSend ($order, $index, $widgetPacks) {
+	// The index represents a position which shows the box that is bigger than the order, and the one that is smaller than the order
+	while ($widgetPacks[$index] > $order) {
+		$index++;
 	}
 	
 	// If the order is the same as one of the boxes, order the box
-	if ($widgetPacks[$iterator] == $order) {
+	if ($widgetPacks[$index] == $order) {
 		return array($order);
 	// The stop condition of the recursive algorithm. When it reaches the end of the recursive calls, it finds the most efficient way to buy boxes. 
 	// Either 2 small ones, or one big one.
-	} elseif ($order - $widgetPacks[$iterator] < $widgetPacks[ARRAYLENGTH - 1]) {
-		$smallPacks = array($widgetPacks[ARRAYLENGTH - 1], $widgetPacks[ARRAYLENGTH - 1]);
-		$mixedPacks = array($widgetPacks[ARRAYLENGTH - 1], $widgetPacks[$iterator]);
+	} elseif ($order - $widgetPacks[$index] < SMALLESTPACK) {
+		$smallPacks = array(SMALLESTPACK, SMALLESTPACK);
+		$mixedPacks = array(SMALLESTPACK, $widgetPacks[$index]);
 		
 		// The function which checks which option orders less extra widgets.
 		$bestPack = betterPack($smallPacks, $mixedPacks, $order);
-		if (($widgetPacks[$iterator - 1] - $order) <= (array_sum($bestPack) - $order)) {
-			return array($widgetPacks[$iterator - 1]);
+		if (($widgetPacks[$index - 1] - $order) <= (array_sum($bestPack) - $order)) {
+			return array($widgetPacks[$index - 1]);
 		} else {
 			return $bestPack;
 		}
 	}
 	
 	// The recursive call of the function
-	$returnedArray = packsToSend($order - $widgetPacks[$iterator], $iterator, $widgetPacks);
+	$returnedArray = packsToSend($order - $widgetPacks[$index], $index, $widgetPacks);
 	
 	// This checks whether buying more small boxes has had less extra widgets, or whether buying a big box will.
-	if (($widgetPacks[$iterator - 1] - $order) <= (array_sum($returnedArray) - $order + $widgetPacks[$iterator])) {
-		return array ($widgetPacks[$iterator - 1]);
+	if (($widgetPacks[$index - 1] - $order) <= (array_sum($returnedArray) - $order + $widgetPacks[$index])) {
+		return array ($widgetPacks[$index - 1]);
 	} else {
-		array_push ($returnedArray, $widgetPacks[$iterator]);
+		array_push ($returnedArray, $widgetPacks[$index]);
 		return $returnedArray;
 	}
 }
